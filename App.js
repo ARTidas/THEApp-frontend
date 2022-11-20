bb7aed98cd5b6e26f66e76744591f1029d0eda73
Veres Zoltán <artidas@artidas.hu>
2022-11-20 16:03:44 +0100

    Initialization

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  StyleSheet, ScrollView, View, Text, Image, TextInput, Button,
+  Component, TouchableOpacity
+} from 'react-native';
+import styles from './App.style.js';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  const [user_email, setUserEmail] = useState('');
+  const [user_password, setUserPassword] = useState('');
+
   return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={ HomeScreen }
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <Text style={styles.titleText}>THEApp</Text>
+
+      <TextInput
+        style={styles.textInput}
+        placeholder="E-mail cím"
+        onChangeText={(user_email) => setUserEmail(user_email)}
+      />
+      <TextInput
+        style={styles.textInput}
+        secureTextEntry={true}
+        autoCapitalize="none"
+        autoCorrect={false}
+        enablesReturnKeyAutomatically
+        placeholder="Jelszó"
+        autoComplete="password"
+        onChangeText={(user_password) => setUserPassword(user_password)}
+      />
+
+      <TouchableOpacity style={styles.buttonInput}>
+        <Text>Bejelentkezés</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.link}>
+        <Text>Elfelejtett jelszó?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.link}>
+        <Text>Nincs fiókod? Regisztráció</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ App.style.js
@@ -0,0 +1,54 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    paddingTop: 20,
+    //fontFamily: "arial, verdana, sans-serif",
+    color: '#333',
+    backgroundColor: '#eee',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  titleText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    paddingBottom: 15
+  },
+  subTitleText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingBottom: 15
+  },
+  textInput: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    width: "90%",
+    backgroundColor: '#fff',
+    margin: 5,
+    padding: 5
+  },
+  buttonInput: {
+    height: 40,
+    borderColor: '#aaa',
+    borderWidth: 1,
+    borderRadius: 5,
+    width: "90%",
+    backgroundColor: '#ccc',
+    margin: 5,
+    padding: 5,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  link: {
+    height: 40,
+    width: "90%",
+    margin: 5,
+    padding: 5,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
